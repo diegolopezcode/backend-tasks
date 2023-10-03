9afb6956fa842d7887523d64367bc9be54cd323b
Diego Lopez <diegolopez.coder@gmail.com>
2023-10-02 19:30:00 -0500

    Add[NestJs]: Add DTOs and update task method
    What: Add DTOs and update task method
    Why: DTOs are used to validate the data that is sent to the server.
    
    * Add DTOs

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Task } from '../schemas/task.schema';
 import { Model } from 'mongoose';
+import { CreateTaskDto } from '../dto/create-task.dto';
+import { UpdateTaskDto } from 'src/dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -15,12 +17,16 @@ export class TasksService {
     return await this.taskModel.findById(id).exec();
   }
 
-  async create(task: Task): Promise<Task> {
+  async create(task: CreateTaskDto): Promise<Task> {
     const newTask = new this.taskModel(task);
     return await newTask.save();
   }
 
   async delete(id: string): Promise<Task> {
     return await this.taskModel.findByIdAndRemove(id);
+  }
+
+  async update(id: string, task: UpdateTaskDto): Promise<Task> {
+    return this.taskModel.findByIdAndUpdate(id, task, { new: true });
   }
 }
