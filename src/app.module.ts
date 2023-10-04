60b1475b71a834c85be8715852e461c7627e9491
Diego Lopez <diegolopez.coder@gmail.com>
2023-10-03 23:47:37 -0500

    Add[NestJs]: Initial Commmit
    What: Start Project with Nestjs
    Why: To practice Nest
    
    001-Start-project

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-tasks'),
+    TasksModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
